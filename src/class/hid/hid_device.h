030b34a914d0c6436d34aa510461724d50f77c1d
gaoyichuan <gaoyichuan000@gmail.com>
2022-05-26 15:52:25 +0800

    fix fido hid report descriptor format

--- src/class/hid/hid_device.h
+++ src/class/hid/hid_device.h
@@ -360,19 +360,19 @@ static inline bool  tud_hid_gamepad_report(uint8_t report_id, int8_t x, int8_t y
     /* Report ID if any */ \
     __VA_ARGS__ \
     /* Usage Data In */ \
-    HID_USAGE_PAGE   ( HID_USAGE_FIDO_INPUT_REPORT_DATA       ) ,\
-    HID_LOGICAL_MIN  ( 0                                      ) ,\
-    HID_LOGICAL_MAX  ( 255                                    ) ,\
-    HID_REPORT_SIZE  ( 8                                      ) ,\
-    HID_REPORT_COUNT ( 64                                     ) ,\
-    HID_INPUT        ( HID_DATA | HID_VARIABLE | HID_ABSOLUTE ) ,\
+    HID_USAGE         ( HID_USAGE_FIDO_INPUT_REPORT_DATA       ) ,\
+    HID_LOGICAL_MIN   ( 0                                      ) ,\
+    HID_LOGICAL_MAX_N ( 255, 2                                 ) ,\
+    HID_REPORT_SIZE   ( 8                                      ) ,\
+    HID_REPORT_COUNT  ( 64                                     ) ,\
+    HID_INPUT         ( HID_DATA | HID_VARIABLE | HID_ABSOLUTE ) ,\
     /* Usage Data Out */ \
-    HID_USAGE_PAGE   ( HID_USAGE_FIDO_OUTPUT_REPORT_DATA      ) ,\
-    HID_LOGICAL_MIN  ( 0                                      ) ,\
-    HID_LOGICAL_MAX  ( 255                                    ) ,\
-    HID_REPORT_SIZE  ( 8                                      ) ,\
-    HID_REPORT_COUNT ( 64                                     ) ,\
-    HID_OUTPUT       ( HID_DATA | HID_VARIABLE | HID_ABSOLUTE ) ,\
+    HID_USAGE         ( HID_USAGE_FIDO_OUTPUT_REPORT_DATA      ) ,\
+    HID_LOGICAL_MIN   ( 0                                      ) ,\
+    HID_LOGICAL_MAX_N ( 255, 2                                 ) ,\
+    HID_REPORT_SIZE   ( 8                                      ) ,\
+    HID_REPORT_COUNT  ( 64                                     ) ,\
+    HID_OUTPUT        ( HID_DATA | HID_VARIABLE | HID_ABSOLUTE ) ,\
   HID_COLLECTION_END \
 
 // HID Generic Input & Output
